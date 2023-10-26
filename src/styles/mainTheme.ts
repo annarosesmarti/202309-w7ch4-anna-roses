a7ee421264a38b98abc4930be436064b982e4478
Anna Roses <anna.roses.marti@gmail.com>
2023-10-26 22:57:20 +0200

    Add variables to styles

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+import "@fontsource/roboto";
+
+const mainTheme: DefaultTheme = {
+  colours: {
+    main: "#432617",
+    background: "#e5dac6",
+  },
+
+  typography: {
+    family: "'Roboto', 'sans-serif'",
+    size: "1rem",
+  },
+};
+
+export default mainTheme;
